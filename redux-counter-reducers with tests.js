1dd656f3d2e742b6714ee014d5d0760ba940a3a2
Daniel Lim <hbt@markhamboards.com>
2016-09-07 12:32:57 -0400

    redux + reducers
    redux + reducers

---
+++ redux-counter-reducers with tests.js
@@ -0,0 +1,22 @@
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+expect(
+  counter(0, { type: 'INCREMENT' })
+).toEqual(1);
+
+expect(
+  counter(1, { type: 'INCREMENT' })
+).toEqual(2);
+
+expect(
+  counter(2, { type: 'DECREMENT' })
+).toEqual(1);
